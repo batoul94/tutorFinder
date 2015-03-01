779b31b3e0905d7736448eabca470b6fdfb064b6
batoul94 <batoul_a94@hotmail.com>
2015-03-01 20:38:04 +0000

    Phonegap Project

---
+++ my-app/platforms/ios/www/footer.js
@@ -0,0 +1,11 @@
+	
+	document.write("<div id=\"wrapper\">");
+	
+		document.write("<div class=\"footer\">");
+		
+			document.write("<img src=\"img/arrow.png\" onclick=\"goBack()\" style=\"position: relative; left: 10%;\">");
+			
+			document.write("<a href=\"index.html\" style=\"position: relative; left: 65%;\"> <img src=\"img/home.png\" ></a>");
+			
+		document.write("</div>");
+	document.write("</div>");
---
+++ my-app/platforms/ios/www/header.js
@@ -0,0 +1,23 @@
+
+document.write("<div id=\"header-wrapper\">");
+  document.write("<header class=\"header-main\">");
+            
+            document.write("<div class=\"page-wrapper\">");
+                document.write("<img src=\"img/log56.jpg\">");
+                    document.write("<button class=\"button-nav-toggle\"><span class=\"icon\">&#9776;</span></button>");
+                    document.write("</div>");
+       document.write("</header>");
+document.write("</div>");
+        document.write("<div class=\"menu\">");
+            document.write("<nav class=\"nav-main\">");
+                document.write("<div class=\"nav-container\">");
+                    document.write("<ul>");
+                        document.write("<li><a href=\"howTo.html\">How To Join Us</a></li>");
+                        document.write("<li> <a href=\"verification.html\">Verification Process</a>");
+                            document.write("<li><a href=\"help.html\">Help</a></li>");
+                            document.write("<li><a href=\"about.html\">About Us</a></li>");
+                            document.write("<li><a href=\"contact.html\">Contact Us</a></li>");
+                            document.write("</ul>");
+                document.write("</div>");
+           document.write("</nav>");
+         document.write("</div>");
