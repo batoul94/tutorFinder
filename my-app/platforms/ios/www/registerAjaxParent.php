dfcec5903f0a357abe163cc84765aaa9469a9b30
Enejda Veliaj <enejdaveliaj@Enejdas-MacBook-Air.local>
2015-03-14 14:07:52 +0000

    login for parents

--- my-app/platforms/ios/www/registerAjaxParent.php
+++ my-app/platforms/ios/www/registerAjaxParent.php
@@ -1,23 +1,7 @@
 <?php
     //this file should be inside mamp htdocs
 	$db = mysqli_connect("localhost","root","root","tutorfinder")or die("EEError " . mysqli_error($db));
-	/*$result = $db->query("SELECT * FROM parentReg");
-
-	$rezultatet = array();
-
- 
-	foreach ($result as $results) {
-             
-		$datas['firstname'] = $results['firstname'];
-		$datas['lastname'] = $results['lastname'];
-		$datas['email'] = $results['email'];
-		$datas['password'] = $results['password'];
-
-		  array_push($rezultatet, $datas);
-
-    }*/
-
-
+	
    $firstname = $_POST['firstname'];
    $lastname = $_POST['lastname'];
    $email = $_POST['email'];
@@ -27,7 +11,5 @@ $result =  $db->query("INSERT INTO parentReg (firstname, lastname, email, passwo
   
 
 
-
-	//echo json_encode($rezultatet);
 
 ?>
---
+++ my-app/www/loginAjaxParent.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    //this file should be inside mamp htdocs
+    $db = mysqli_connect("localhost","root","root","tutorfinder")or die("EEError " . mysqli_error($db));
+    
+    
+    if(isset($_POST['email']) && isset($_POST['password'])) {
+
+        $email=mysqli_real_escape_string($db,$_POST['email']);
+        $password=mysqli_real_escape_string($db,$_POST['password']);
+        $result=mysqli_query($db,"SELECT firstname FROM parentReg WHERE email='$email' AND password='$password'");
+        $count=mysqli_num_rows($result);
+        $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        if($count==1) {
+
+            $_SESSION['login_parent']=$row['id'];
+            echo $row['firstname'];
+            //header("location: profile.php");
+
+        }
+
+    }
+?>
